09cb03b9a8792a26e396bbfc8c97220dcb99f1b6
Felipe Augusto <felipe_spinassi@hotmail.com>
2025-02-05 15:26:25 -0300

    fix: fix fetcher error

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,10 +1,13 @@
 import { getTokemFromCookie } from "./getTokenFromCookie";
 
-export const fetcher = async (...args: any) => {
+export const fetcher = async (...args: [RequestInfo, RequestInit?]) => {
   const token = await getTokemFromCookie();
 
-  return fetch(...args, {
+  const [url, options] = args;
+  return fetch(url, {
+    ...options,
     headers: {
+      ...options?.headers,
       Authorization: `Bearer ${token!.value}`,
     },
   }).then((res) => res.json());
